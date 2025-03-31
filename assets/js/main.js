f904b08e05bfb54e35258f65845c57a833319e24
Mohammadhajivandi <hajywandymohammad3@gmail.com>
2025-03-31 11:24:06 +0330

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,8 +119,5 @@ ScrollReveal().reveal('.contact-inner');
 
 document.addEventListener('DOMContentLoaded', function () {
     let preloader = document.getElementById('preloader')
-    let content = document.getElementById('content')
-
     preloader.style.display = 'none';
-    content.style.display = 'block'
 });
